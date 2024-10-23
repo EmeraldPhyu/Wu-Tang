98851d2e2a17eac9231903e41acd216e63a76781
Emerald <phyuhninhtway@gmail.com>
2024-10-23 16:45:12 +0800

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by Emerald on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
